850317b36719bd9051246eac68cc44ea9b6f8ce6
giova <giova@DESKTOP-QUF6PHS.utsarr.net>
2022-04-20 17:37:47 -0500

    test branch gio

--- Parking App/src/application/Main.java
+++ Parking App/src/application/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			// test branch
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
