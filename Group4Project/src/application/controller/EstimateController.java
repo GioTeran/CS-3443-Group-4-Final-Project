9b85e651f4ee8c9f6fc6748ef9fe46727c33884f
Shandra-L <43977445+Shandra-L@users.noreply.github.com>
2022-04-23 10:39:19 -0500

    Newest one

--- Group4Project/src/application/controller/EstimateController.java
+++ Group4Project/src/application/controller/EstimateController.java
@@ -6,10 +6,12 @@ package application.controller;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -17,7 +19,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class EstimateController {
+public class EstimateController implements Initializable{
 
 	@FXML
     private Button BackButton;
@@ -33,7 +35,7 @@ public class EstimateController {
 
     @FXML
     void OkButtonClicked(ActionEvent event) throws IOException {
-    	URL url = new File("src/Cost5.5.fxml").toURI().toURL();
+    	URL url = new File("src/application/Cost.fxml").toURI().toURL();
    	 	InfoPane = FXMLLoader.load(url);// pane you are GOING TO
         Scene scene = new Scene(InfoPane);// pane you are GOING TO show
         Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
@@ -43,12 +45,15 @@ public class EstimateController {
     }
     @FXML
     void BackButtonClicked(ActionEvent event) throws IOException {
-    	URL url = new File("src/DaysHours4.fxml").toURI().toURL();
+    	URL url = new File("src/DaysHours.fxml").toURI().toURL();
    	 	InfoPane = FXMLLoader.load(url);// pane you are GOING TO
         Scene scene = new Scene(InfoPane);// pane you are GOING TO show
         Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
         window.setScene(scene);
         window.show();
 
+    }
+    public void initialize(URL arg0, ResourceBundle arg1){
+    	InfoField.setText("DON'T FORGET TO PUT INFO ABOUT PERMITS");
     }
 }
