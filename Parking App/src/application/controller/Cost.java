c9917f75915e1994234c4890347ff5d9ace65cc5
retrouvm <67712339+retrouvm@users.noreply.github.com>
2022-04-24 03:47:11 -0500

    Add files via upload

---
+++ Parking App/src/application/controller/Cost.java
@@ -0,0 +1,83 @@
+// controller for cost5.5.fxml scene
+package application.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import application.model.*;
+
+public class Cost implements Initializable{
+
+	@FXML
+	private Button BackButton;
+
+    @FXML
+    private AnchorPane CostPane;
+
+    @FXML
+    private Button NextButton;
+
+    @FXML
+    private TextArea OtherPermits1;
+
+    @FXML
+    private TextArea OtherPermits2;
+
+    @FXML
+    private TextArea OtherPermits3;
+
+    @FXML
+    private TextArea OtherPermits4;
+
+    @FXML
+    private TextArea OtherPermits5;
+
+    @FXML
+    private TextArea OtherPermits6;
+
+    @FXML
+    private TextArea PermitCost;
+
+    @FXML
+    void NextButtonClicked(ActionEvent event) throws IOException {
+    	URL url = new File("src/ParkingModel6.fxml").toURI().toURL();
+   	 	CostPane = FXMLLoader.load(url);// pane you are GOING TO
+        Scene scene = new Scene(CostPane);// pane you are GOING TO show
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
+        window.setScene(scene);
+        window.show();
+
+    }
+    @FXML
+    void BackButtonClicked(ActionEvent event) throws IOException {
+    	URL url = new File("src/Estimates5.fxml").toURI().toURL();
+   	 	CostPane = FXMLLoader.load(url);// pane you are GOING TO
+        Scene scene = new Scene(CostPane);// pane you are GOING TO show
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
+        window.setScene(scene);
+        window.show();
+
+    }
+
+    Estimate estimate = new Estimate();
+    public void initialize(URL arg0, ResourceBundle arg1){
+    	try {
+			PermitCost.setText(estimate.finalEstimate());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
---
+++ Parking App/src/application/controller/EstimateController.java
@@ -0,0 +1,59 @@
+/**
+ * This controller is for the Estimate fxml file
+ */
+package application.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class EstimateController implements Initializable{
+
+	@FXML
+    private Button BackButton;
+	
+    @FXML
+    private TextField InfoField;
+
+    @FXML
+    private AnchorPane InfoPane;
+
+    @FXML
+    private Button OkButton;
+
+    @FXML
+    void OkButtonClicked(ActionEvent event) throws IOException {
+    	URL url = new File("src/Cost.fxml").toURI().toURL();
+   	 	InfoPane = FXMLLoader.load(url);// pane you are GOING TO
+        Scene scene = new Scene(InfoPane);// pane you are GOING TO show
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
+        window.setScene(scene);
+        window.show();
+
+    }
+    @FXML
+    void BackButtonClicked(ActionEvent event) throws IOException {
+    	URL url = new File("src/DaysHours.fxml").toURI().toURL();
+   	 	InfoPane = FXMLLoader.load(url);// pane you are GOING TO
+        Scene scene = new Scene(InfoPane);// pane you are GOING TO show
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
+        window.setScene(scene);
+        window.show();
+
+    }
+    public void initialize(URL arg0, ResourceBundle arg1){
+    	InfoField.setText("DON'T FORGET TO PUT INFO ABOUT PERMITS");
+    }
+}
