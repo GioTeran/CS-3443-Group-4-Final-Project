e9efff50795b46614ebe44a95f538d837ecf61c8
Shandra-L <43977445+Shandra-L@users.noreply.github.com>
2022-04-24 15:32:59 -0500

    fixed errors for estimate

--- Parking App/src/application/model/Estimate.java
+++ Parking App/src/application/model/Estimate.java
@@ -1,15 +1,18 @@
 package application.model;
 
 import java.io.File;
-//import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-//import java.util.Iterator;
+import java.util.Iterator;
 import java.util.Scanner;
 
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Estimate {
-	public String finalEstimate() throws IOException {
-		File file = new File("src/data/PermitSelection.txt");
+	public void finalEstimate(TextArea textfield) throws IOException {
+		File file = new File("src/data/permitSelection.txt");
 		File file2 = new File("src/data/Status.txt");
 		File file3 = new File("src/data/DaysHours.txt");
 		
@@ -24,7 +27,7 @@ public class Estimate {
 		String permPeriod = "";
 		
 		double earliest;
-		//double latest;
+		double latest;
 		
 		int numDays;
 		
@@ -42,7 +45,7 @@ public class Estimate {
 			daysHours = scanner3.nextLine();
 		}
 		
-		String split[] = selection.split(",", 3);
+		String split[] = selection.split(", ", 3);
 		
 		permType = split[0];
 		permPeriod = split[1];
@@ -51,73 +54,92 @@ public class Estimate {
 		
 		numDays = Integer.valueOf(split1[0]);
 		earliest = Double.valueOf(split1[1]);
-		//latest = Double.valueOf(split1[2]);
-		scanner1.close();
-		scanner2.close();
-		scanner3.close();
+		latest = Double.valueOf(split1[2]);
+		
 		for(int i = 0; i < numDays; i++) {
 			days.add(split1[3+i]);
 		}
 		
-		if(status == "student" && permPeriod == "yearly") {
+		
+		
+		if(status.equals("student") && permPeriod.equals("year")) {
+			System.out.println("IM IN");
 			if(permType.equals("surface") && (earliest >= 16.5)) {
-				return "The most cost efficient permit for your schedule and prefrences is the Twilight Permit."+"\n"+"It costs $82 per academic year and allows parking in Unmarked Commuter Student spaces from 4:30 pm – 6:00 am weekdays & all day weekends.";
+				textfield.setText("The most cost efficient permit for your schedule and prefrences is the Twilight Permit. It costs $82 per academic year and allows parking in Unmarked Commuter Student spaces from 4:30 pm – 6:00 am weekdays & all day weekends.");
+				return;
 			}
 			else if(permType.equals("surface") && (earliest < 16.5)) {
-				return "The most cost efficient permit for your schedule and prefrences is the Commuter Student Permit."+"\n"+"It costs $170 per academic year and allows parking in Unmarked Commuter Student spaces anytime and Employee B spaces from 4:30 pm – 6:00 am weekdays & all day weekends.";
+				textfield.setText("The most cost efficient permit for your schedule and prefrences is the Commuter Student Permit. It costs $170 per academic year and allows parking in Unmarked Commuter Student spaces anytime and Employee B spaces from 4:30 pm – 6:00 am weekdays & all day weekends.");
+				return;
 			}
 		}
-		else if(status == "student" && permPeriod == "day" && permType == "surface") {
-			return "The most cost efficient permit for your schedule and prefrences is the Commuter Single-Use Daily Parking Permit."+"\n"+"It cost $7 per each permit and allows parking in Unmarked Commuter Student spaces.";
+		else if(status.equals("student") && permPeriod.equals("day") && permType.equals("surface")) {
+			textfield.setText("The most cost efficient permit for your schedule and prefrences is the Commuter Single-Use Daily Parking Permit. It cost $7 per each permit and allows parking in Unmarked Commuter Student spaces.");
+			return;
 		}
-		else if(status == "student" && permPeriod == "hourly" && permType == "garage") {
-			return "The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.";
+		else if(status.equals("student") && permPeriod.equals("hourly") && permType.equals("garage")) {
+			textfield.setText("The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.");
+			return;
 		}
-		else if(status == "student" && permPeriod == "hourly" && permType == "surface") {
-			return "The most cost efficient permit for your schedule and prefrences is the Commuter Single-Use Daily Parking Permit."+"\n"+"It cost $7 per each permit and allows parking in Unmarked Commuter Student spaces.";
+		else if(status.equals("student") && permPeriod.equals("hourly") && permType.equals("surface")) {
+			textfield.setText("The most cost efficient permit for your schedule and prefrences is the Commuter Single-Use Daily Parking Permit. It cost $7 per each permit and allows parking in Unmarked Commuter Student spaces.");
+			return;
 		}
-		else if(status == "student" && permPeriod == "day" && permType == "garage") {
-			return "The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.";
+		else if(status.equals("student") && permPeriod.equals("day") && permType.equals("garage")) {
+			textfield.setText("The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.");
+			return;
 		}
-		else if(status == "student" && permPeriod == "year" && permType == "garage") {
+		else if(status.equals("student") && permPeriod.equals("year") && permType.equals("garage")) {
 			if(earliest >= 16.5) {
-				return "The most cost efficient parking for your schedule and prefrences is to get a Night Only Permit for either the Xiemnes, Baurle, Tobin, or Downtown Campus garages."+"\n"+"Each costs only $264 per academic year.";
+				textfield.setText("The most cost efficient parking for your schedule and prefrences is to get a Night Only Permit for either the Xiemnes, Baurle, Tobin, or Downtown Campus garages. Each costs only $264 per academic year.");
+				return;
 			}
 			else {
-				return "The most cost efficient parking for your schedule and prefrences is to get a Garage Permit for either the Xiemnes, Baurle, Tobin, or Downtown Campus garages."+"\n"+"Each costs only $864 per academic year.";
+				textfield.setText("The most cost efficient parking for your schedule and prefrences is to get a Garage Permit for either the Xiemnes, Baurle, Tobin, or Downtown Campus garages. Each costs only $864 per academic year.");
+				return;
 			}
 		}
-		else if(status == "staff" && permPeriod == "day" && permType == "surface") {
-			return "The most cost efficient parking for your schedule and prefrences is to buy the Employee A Single-Use Daily Parking."+"\n"+"It costs only $7 per permit and allows parking in Employee A or Employee B  spaces and Unmarked Commuter Student spaces.";
+		else if(status.equals("staff") && permPeriod.equals("day") && permType.equals("surface")) {
+			textfield.setText("The most cost efficient parking for your schedule and prefrences is to buy the Employee A Single-Use Daily Parking. It costs only $7 per permit and allows parking in Employee A or Employee B  spaces and Unmarked Commuter Student spaces.");
+			return;
 		}
-		else if(status == "staff" && permPeriod == "day" && permType == "garage") {
-			return "The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.";
+		else if(status.equals("staff") && permPeriod.equals("day") && permType.equals("garage")) {
+			textfield.setText("The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.");
+			return;
 		}
-		else if(status == "staff" && permPeriod == "hourly" && permType == "surface") {
-			return "The most cost efficient parking for your schedule and prefrences is to buy the Employee A Single-Use Daily Parking."+"\n"+"It costs only $7 per permit and allows parking in Employee A or Employee B  spaces and Unmarked Commuter Student spaces.";
+		else if(status.equals("staff") && permPeriod.equals("hourly") && permType.equals("surface")) {
+			textfield.setText("The most cost efficient parking for your schedule and prefrences is to buy the Employee A Single-Use Daily Parking. It costs only $7 per permit and allows parking in Employee A or Employee B  spaces and Unmarked Commuter Student spaces.");
 		}
-		else if(status == "staff" && permPeriod == "hourly" && permType == "garage") {
-			return "The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.";
+		else if(status.equals("staff") && permPeriod.equals("hourly") && permType.equals("garage")) {
+			textfield.setText("The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.");
+			return;
 		}
-		else if(status == "staff" && permPeriod == "yearly" && permType == "surface") {
-			return "The most cost efficient parking for your schedule and prefrences is to get the Employee B parking."+"\n"+"It costs $203 and allows parking in Employee B spaces, Unmarked Commuter Student spaces, and Employee A spaces spaces from 4:30 pm – 6:00 am weekdays & all day weekends";
+		else if(status.equals("staff") && permPeriod.equals("yearly") && permType.equals("surface")) {
+			textfield.setText("The most cost efficient parking for your schedule and prefrences is to get the Employee B parking. It costs $203 and allows parking in Employee B spaces, Unmarked Commuter Student spaces, and Employee A spaces spaces from 4:30 pm – 6:00 am weekdays & all day weekends");
+			return;
 		}
-		else if(status == "staff" && permPeriod == "yearly" && permType == "garage") {
+		else if(status.equals("staff") && permPeriod.equals("yearly") && permType.equals("garage")) {
 			if(earliest >= 16.5) {
-				return "The most cost efficient parking for your schedule and prefrences is to get a Employee Night Only Garage Permit."+"\n"+"It costs $268 and are available for the Ximenes, Baurle, and Tobin garages.";	
+				textfield.setText("The most cost efficient parking for your schedule and prefrences is to get a Employee Night Only Garage Permit. It costs $268 and are available for the Ximenes, Baurle, and Tobin garages.");	
+				return;
 			}
 			else {
-				return "The most cost efficient parking for your schedule and prefrences is to get an Employee Garage Permit."+"\n"+"It costs $858 and are available for the Ximenes, Baurle, and Tobin garages.";
+				textfield.setText("The most cost efficient parking for your schedule and prefrences is to get an Employee Garage Permit. It costs $858 and are available for the Ximenes, Baurle, and Tobin garages.");
+				return;
 			}
 		}
-		else if(status == "guest" && permType == "garage") {
-			return "The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.";
+		else if(status.equals("guest") && permType.equals("garage")) {
+			textfield.setText("The most cost efficient parking for your schedule and prefrences is to park in either the Xiemnes, Baurle, Tobin, or Downtown Campus garages for only $2.50 per hour.");
+			return;
 		}
-		else if(status == "guest" && permType == "day") {
-			return "The most cost efficient permit for your schedule and prefrences is the Commuter Single-Use Daily Parking Permit."+"\n"+"It cost $7 per each permit and allows parking in Unmarked Commuter Student spaces.";
+		else if(status.equals("guest") && permType .equals("day")) {
+			textfield.setText("The most cost efficient permit for your schedule and prefrences is the Commuter Single-Use Daily Parking Permit. It cost $7 per each permit and allows parking in Unmarked Commuter Student spaces.");
+			return;
 		}
 		
-		return "The most cost efficient permit for your schedule and prefrences is the Commuter Student Permit."+"\n"+"It costs $170 per academic year and allows parking in Unmarked Commuter Student spaces anytime and Employee B spaces from 4:30 pm – 6:00 am weekdays & all day weekends.";
+		scanner1.close();
+		scanner2.close();
+		scanner3.close();
 		
 	}
 }
