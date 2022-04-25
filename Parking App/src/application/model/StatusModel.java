3a5a4ecd62775c3b6cefb4979c4afb614ceeb172
Shandra-L <43977445+Shandra-L@users.noreply.github.com>
2022-04-24 23:49:11 -0500

    added comments

--- Parking App/src/application/model/StatusModel.java
+++ Parking App/src/application/model/StatusModel.java
@@ -1,3 +1,4 @@
+//model for Status.FXML file
 package application.model;
 
 import java.io.File;
@@ -5,6 +6,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class StatusModel {
+	/*
+         * Name: saveStatus
+         * Description: saves the user's status to Status.txt file
+   	 * Parameters: status (String)
+   	 * returns: nothing
+       */
 	public void saveStatus(String status) throws IOException {
 		File file = new File("src/data/Status.txt");
 		FileWriter myWriter = new FileWriter(file , true);
