705c5e62bfb81ae12c96d0326e15624cfe56baf7
Shandra-L <43977445+Shandra-L@users.noreply.github.com>
2022-04-24 20:06:56 -0500

    now includes parking number

--- Parking App/src/application/model/SelectionModel.java
+++ Parking App/src/application/model/SelectionModel.java
@@ -3,13 +3,34 @@ package application.model;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
 
 public class SelectionModel {
 	public void saveSelection(String type, String period) throws IOException {
-		File file = new File("src/data/PermitSelection.txt");
+		File file = new File("src/data/permitSelection.txt");
 		FileWriter myWriter = new FileWriter(file , true);
+		Random rand = new Random();
+		int low = 100;
+		int high = 1000;
+		int parkingNum = rand.nextInt(high - low) + low;
+		Scanner scanner = new Scanner(file);
 		
-		myWriter.write(type + ", " + period + "\n");
+		boolean repeat = false;
+		
+		if(scanner.hasNextLine() && scanner.hasNextInt()) {
+			while (scanner.hasNextLine()) {
+				int temp;
+				
+				temp = scanner.nextInt();
+				
+				if( temp == parkingNum) {
+					parkingNum += (rand.nextInt(high-low) + low) % 1000;
+				}
+				
+			}
+		}
+		myWriter.write(type + ", " + period + ", " + parkingNum + "\n");
 		
 		myWriter.close();
 	}
